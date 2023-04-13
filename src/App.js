6758b41dba4f73cdb1408e0d152ac7b60974c8b7
niloferabdul4 <niloferabdul4@gmail.com>
2023-04-13 08:43:08 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Login from './Pages/Login/Login';
 import Dashboard from './Pages/Dashboard/Dashboard';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './Context/AuthContext';
 
